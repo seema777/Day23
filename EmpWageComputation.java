029bb4c17d683c41a692f663b38e6045130dc122
seema777 <navaghareseema3@gmail.com>
2020-08-09 02:06:55 +1300

    Emp Attendance

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,3 +5,23 @@ public class EmpWageComputation
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 	}
 }
+
+
+
+public class EmpwageBuilder
+{
+        public static void main(String[] args)
+        {
+                int IS_FULL_TIME=1;
+                double empcheck=Math.floor(Math.random()*10)%2;
+                if(empcheck == IS_FULL_TIME)
+                        {
+                                System.out.println("Employee is Persent");
+                        }
+                        else
+                                System.out.println("Employee is Absent");
+        }
+}
+
+
+
