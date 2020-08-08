c042414562a61989f6cadfaedad0132dd13ab43f
seema777 <navaghareseema3@gmail.com>
2020-08-09 02:20:51 +1300

    Emp Wage For Month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -111,6 +111,43 @@ public class EmpwageBuilder
 
 
 
+public class EmpwageBuilder
+{
+                public static final int IS_FULL_TIME=1;
+                public static final int IS_PART_TIME=2;
+                public static final int EMP_RATE_PER_HOUR=20;
+                public static final int NUM_OF_WORKING_DAYS=2;
+        public static void main(String[] args)
+        {
+                int empHrs=0;
+                int empWage=0;
+                int totalEmpwage=0;
+                for(int day=1;day<= NUM_OF_WORKING_DAYS;day++)
+                {
+                        double empcheck=Math.floor(Math.random()*10)%3;
+                        switch((int)empcheck)
+                        {
+                        case IS_PART_TIME:
+                                empHrs=4;
+                                 break;
+                        case IS_FULL_TIME:
+                                empHrs=8;
+                                break;
+                         default:
+                                empHrs=0;
+
+                                }
+                                empWage=empHrs*EMP_RATE_PER_HOUR;
+                                totalEmpwage+=empWage;
+                                System.out.println("Emp Wage on day "+day+" is  : "+empWage);
+                                }
+                                System.out.println("Employee wage"+totalEmpwage);
+        }
+}
+
+
+
+
 
 
 
