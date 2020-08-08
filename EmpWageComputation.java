9f2966da4c2516d6520872136d8b4f7e4c6fc282
seema777 <navaghareseema3@gmail.com>
2020-08-09 02:17:51 +1300

    Emp JobType Using Switch

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -49,6 +49,9 @@ public class EmpwageBuilder
 
 
 
+
+
+
 public class EmpwageBuilder
 {
         public static void main(String[] args)
@@ -75,6 +78,37 @@ public class EmpwageBuilder
                                 System.out.println("Employee Wage "+empWage);
         }
 }
+
+
+
+public class EmpwageBuilder
+{
+        public static void main(String[] args)
+        {
+                 final int IS_FULL_TIME=1;
+                 final int IS_PART_TIME=2;
+
+                 final int EMP_RATE_PER_HOUR=20;
+                int empHrs=0;
+                int empWage=0;
+                        double empcheck=Math.floor(Math.random()*10)%3;
+                        switch((int)empcheck)
+                        {
+                        case IS_PART_TIME:
+                                empHrs=4;
+                                 break;
+                        case IS_FULL_TIME:
+                                empHrs=8;
+                                break;
+                         default:
+                                empHrs=0;
+
+                                }
+                                empWage=empHrs*EMP_RATE_PER_HOUR;
+                                System.out.println("Employee Wage "+empWage);
+        }
+}
+
 
 
 
